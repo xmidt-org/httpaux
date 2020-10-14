6783f679cc389b03142689c810acbf7c4a85e678
John Bass <johnbass1017@yahoo.com>
2020-10-13 23:32:37 -0700

    fleshed out busy tests

--- busy_test.go
+++ busy_test.go
@@ -1,7 +1,10 @@
 package httpbuddy
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -71,5 +74,91 @@ func TestMaxRequestLimiter(t *testing.T) {
 	t.Run("Limited", testMaxRequestLimiterLimited)
 }
 
+func testBusyLimited(t *testing.T, onBusy http.Handler, expectedBusyCode int) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		waiting  = make(chan struct{})
+		unpaused = make(chan struct{})
+
+		handlerPause = make(chan struct{}, 1)
+		handler      = http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+			<-handlerPause
+			response.WriteHeader(298)
+		})
+
+		decorated = Busy{
+			Limiter: &MaxRequestLimiter{
+				MaxRequests: 1,
+			},
+			OnBusy: onBusy,
+		}.Then(handler)
+	)
+
+	require.NotNil(decorated)
+
+	handlerPause <- struct{}{}
+	first := httptest.NewRecorder()
+	decorated.ServeHTTP(first, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(298, first.Code)
+
+	go func() {
+		defer close(unpaused)
+
+		// pause this request to simulate a long-running ServeHTTP call
+		response := httptest.NewRecorder()
+		close(waiting)
+		decorated.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+		assert.Equal(298, response.Code)
+	}()
+
+	select {
+	case <-waiting:
+		// passing
+	case <-time.After(time.Second):
+		require.Fail("goroutine did not start waiting")
+	}
+
+	second := httptest.NewRecorder()
+	decorated.ServeHTTP(second, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(expectedBusyCode, second.Code)
+
+	handlerPause <- struct{}{}
+	select {
+	case <-unpaused:
+		// passing
+	case <-time.After(time.Second):
+		require.Fail("the paused request did not complete")
+	}
+
+	handlerPause <- struct{}{}
+	third := httptest.NewRecorder()
+	decorated.ServeHTTP(third, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(298, third.Code)
+}
+
+func testBusyUnlimited(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		handler   = ConstantHandler{StatusCode: 222}
+		decorated = Busy{}.Then(handler)
+	)
+
+	require.NotNil(decorated)
+	assert.Equal(handler, decorated)
+}
+
 func TestBusy(t *testing.T) {
+	t.Run("Unlimited", testBusyUnlimited)
+
+	t.Run("LimitedDefaultOnBusy", func(t *testing.T) {
+		testBusyLimited(t, nil, http.StatusServiceUnavailable)
+	})
+
+	t.Run("LimitedCustomOnBusy", func(t *testing.T) {
+		testBusyLimited(t, ConstantHandler{StatusCode: 517}, 517)
+	})
 }
