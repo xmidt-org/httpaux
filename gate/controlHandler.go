82eefc111fcae8750d300c755eb8572a8741bd24
John Bass <johnbass1017@yahoo.com>
2020-11-24 21:37:07 -0800

    control handler tests

--- gate/controlHandler.go
+++ gate/controlHandler.go
@@ -20,23 +20,23 @@ const (
 type ControlHandler struct {
 	// StateChange is the required strategy for determining how to change a gate
 	// given an HTTP request.  This closure can examine any aspect of the request, e.g.
-	// URL parameters, URI, etc, to determine what state change should occur.
+	// URL parameters, request URI, etc, to determine what state change should occur.
 	//
 	// This closure can return StateNoChange to indicate that the handler should do nothing
-	// to a gate.  This may be true due to a security permissions check, as one example.
+	// to a gate.  For example, the request may fail a security permissions check.
 	StateChange func(*http.Request) StateChange
 
-	// Control is the required gate control instance used to open and close the gate
-	Control Control
+	// Gate is the required gate control instance used to open and close the gate
+	Gate Control
 }
 
 // ServeHTTP invokes the StateChange closure and takes the appropriate action
 func (ch ControlHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	switch ch.StateChange(request) {
 	case StateOpen:
-		ch.Control.Open()
+		ch.Gate.Open()
 
 	case StateClose:
-		ch.Control.Close()
+		ch.Gate.Close()
 	}
 }
---
+++ gate/controlHandler_test.go
@@ -0,0 +1,80 @@
+package httpaux
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type ControlHandlerTestSuite struct {
+	suite.Suite
+}
+
+func (suite *ControlHandlerTestSuite) noChange(*http.Request) StateChange   { return StateNoChange }
+func (suite *ControlHandlerTestSuite) stateOpen(*http.Request) StateChange  { return StateOpen }
+func (suite *ControlHandlerTestSuite) stateClose(*http.Request) StateChange { return StateClose }
+
+func (suite *ControlHandlerTestSuite) Test() {
+	cases := []struct {
+		stateChange func(*http.Request) StateChange
+		gate        Interface
+		expectOpen  bool
+	}{
+		{
+			stateChange: suite.noChange,
+			gate:        New(Config{}),
+			expectOpen:  true,
+		},
+		{
+			stateChange: suite.stateOpen,
+			gate:        New(Config{}),
+			expectOpen:  true,
+		},
+		{
+			stateChange: suite.stateOpen,
+			gate: New(Config{
+				InitiallyClosed: true,
+			}),
+			expectOpen: true,
+		},
+		{
+			stateChange: suite.noChange,
+			gate: New(Config{
+				InitiallyClosed: true,
+			}),
+			expectOpen: false,
+		},
+		{
+			stateChange: suite.stateClose,
+			gate:        New(Config{}),
+			expectOpen:  false,
+		},
+		{
+			stateChange: suite.stateClose,
+			gate: New(Config{
+				InitiallyClosed: true,
+			}),
+			expectOpen: false,
+		},
+	}
+
+	for i, testCase := range cases {
+		suite.Run(strconv.Itoa(i), func() {
+			response := httptest.NewRecorder()
+			ControlHandler{
+				StateChange: testCase.stateChange,
+				Gate:        testCase.gate,
+			}.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+
+			suite.Equal(200, response.Code)
+			suite.Equal(testCase.expectOpen, testCase.gate.IsOpen())
+		})
+	}
+}
+
+func TestControlHandler(t *testing.T) {
+	suite.Run(t, new(ControlHandlerTestSuite))
+}
