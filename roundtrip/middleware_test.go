1ac15628927cad99b72788a6064362fe46f7c4d2
John Bass <johnbass1017@yahoo.com>
2020-11-27 13:48:04 -0800

    fleshed out the basic tests

--- roundtrip/middleware_test.go
+++ roundtrip/middleware_test.go
@@ -1,1 +1,31 @@
 package roundtrip
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConstructor(t *testing.T) {
+	var (
+		assert   = assert.New(t)
+		require  = require.New(t)
+		expected = &http.Transport{
+			MaxResponseHeaderBytes: 1234,
+		}
+
+		called bool
+		c      Constructor = func(actual http.RoundTripper) http.RoundTripper {
+			called = true
+			assert.Equal(expected, actual)
+			return actual
+		}
+	)
+
+	decorated := c.Then(expected)
+	assert.True(called)
+	require.NotNil(decorated)
+	assert.Equal(expected, decorated)
+}
--- roundtrip/roundTripper_test.go
+++ roundtrip/roundTripper_test.go
@@ -1,6 +1,7 @@
 package roundtrip
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -31,56 +32,173 @@ func TestFunc(t *testing.T) {
 	assert.Equal(211, response.StatusCode)
 }
 
-func TestConstructor(t *testing.T) {
+func TestCloseIdlerFunc(t *testing.T) {
 	var (
-		assert   = assert.New(t)
-		require  = require.New(t)
-		expected = &http.Transport{
-			MaxResponseHeaderBytes: 1234,
-		}
+		assert = assert.New(t)
 
 		called bool
-		c      Constructor = func(actual http.RoundTripper) http.RoundTripper {
+		c      CloseIdlerFunc = func() {
 			called = true
-			assert.Equal(expected, actual)
-			return actual
 		}
 	)
 
-	decorated := c.Then(expected)
+	c.CloseIdleConnections()
 	assert.True(called)
-	require.NotNil(decorated)
-	assert.Equal(expected, decorated)
 }
 
 type CloseIdleConnectionsTestSuite struct {
 	suite.Suite
-	roundTripper *mockRoundTripper
-	closeIdler   *mockRoundTripperCloseIdler
-}
-
-var _ suite.SetupTestSuite = (*CloseIdleConnectionsTestSuite)(nil)
-var _ suite.TearDownTestSuite = (*CloseIdleConnectionsTestSuite)(nil)
-
-func (suite *CloseIdleConnectionsTestSuite) SetupTest() {
-	suite.roundTripper = new(mockRoundTripper)
-	suite.closeIdler = new(mockRoundTripperCloseIdler)
-}
-
-func (suite *CloseIdleConnectionsTestSuite) TearDownTest() {
-	suite.roundTripper.AssertExpectations(suite.T())
-	suite.closeIdler.AssertExpectations(suite.T())
 }
 
 func (suite *CloseIdleConnectionsTestSuite) TestRoundTripper() {
-	CloseIdleConnections(suite.roundTripper)
+	m := new(mockRoundTripper)
+	CloseIdleConnections(m)
+	m.AssertExpectations(suite.T())
 }
 
 func (suite *CloseIdleConnectionsTestSuite) TestCloseIdler() {
-	suite.closeIdler.On("CloseIdleConnections").Once()
-	CloseIdleConnections(suite.closeIdler)
+	m := new(mockRoundTripperCloseIdler)
+	m.On("CloseIdleConnections").Once()
+	CloseIdleConnections(m)
+	m.AssertExpectations(suite.T())
 }
 
 func TestCloseIdleConnections(t *testing.T) {
 	suite.Run(t, new(CloseIdleConnectionsTestSuite))
+}
+
+type PreserveCloseIdlerTestSuite struct {
+	suite.Suite
+	request  *http.Request
+	response *http.Response
+	err      error
+}
+
+var _ suite.BeforeTest = (*PreserveCloseIdlerTestSuite)(nil)
+
+func (suite *PreserveCloseIdlerTestSuite) BeforeTest(_, testName string) {
+	suite.request = httptest.NewRequest("GET", "/"+testName, nil)
+	suite.response = &http.Response{
+		StatusCode: 214,
+		Header: http.Header{
+			"X-Test": {testName},
+		},
+	}
+
+	suite.err = fmt.Errorf("expected error: %s", testName)
+}
+
+func (suite *PreserveCloseIdlerTestSuite) TestNoCloseIdler() {
+	var (
+		next      = new(mockRoundTripper)
+		decorator = PreserveCloseIdler(
+			next,
+			Func(func(r *http.Request) (*http.Response, error) {
+				return next.RoundTrip(r)
+			}),
+		)
+	)
+
+	suite.Require().NotNil(decorator)
+	next.On("RoundTrip", suite.request).Once().Return(suite.response, suite.err)
+
+	response, err := decorator.RoundTrip(suite.request)
+	suite.Equal(suite.response, response)
+	suite.Equal(suite.err, err)
+
+	_, ok := decorator.(CloseIdler)
+	suite.False(ok)
+
+	next.AssertExpectations(suite.T())
+}
+
+func (suite *PreserveCloseIdlerTestSuite) TestDecoratedCloseIdler() {
+	var (
+		next       = new(mockRoundTripper)
+		closeIdler = new(mockRoundTripperCloseIdler)
+
+		decorator = PreserveCloseIdler(
+			next,
+			Decorator{
+				RoundTripper: Func(func(r *http.Request) (*http.Response, error) {
+					return next.RoundTrip(r)
+				}),
+				CloseIdler: closeIdler,
+			},
+		)
+	)
+
+	suite.Require().NotNil(decorator)
+	next.On("RoundTrip", suite.request).Once().Return(suite.response, suite.err)
+	closeIdler.On("CloseIdleConnections").Once()
+
+	response, err := decorator.RoundTrip(suite.request)
+	suite.Equal(suite.response, response)
+	suite.Equal(suite.err, err)
+
+	suite.Require().Implements((*CloseIdler)(nil), decorator)
+	decorator.(CloseIdler).CloseIdleConnections()
+
+	next.AssertExpectations(suite.T())
+	closeIdler.AssertExpectations(suite.T())
+}
+
+func (suite *PreserveCloseIdlerTestSuite) TestNextCloseIdler() {
+	var (
+		next      = new(mockRoundTripperCloseIdler)
+		decorator = PreserveCloseIdler(
+			next,
+			Func(func(r *http.Request) (*http.Response, error) {
+				return next.RoundTrip(r)
+			}),
+		)
+	)
+
+	suite.Require().NotNil(decorator)
+	next.On("RoundTrip", suite.request).Once().Return(suite.response, suite.err)
+	next.On("CloseIdleConnections").Once()
+
+	response, err := decorator.RoundTrip(suite.request)
+	suite.Equal(suite.response, response)
+	suite.Equal(suite.err, err)
+
+	suite.Require().Implements((*CloseIdler)(nil), decorator)
+	decorator.(CloseIdler).CloseIdleConnections()
+
+	next.AssertExpectations(suite.T())
+}
+
+func (suite *PreserveCloseIdlerTestSuite) TestNextDecorator() {
+	var (
+		next       = new(mockRoundTripper)
+		closeIdler = new(mockRoundTripperCloseIdler)
+
+		decorator = PreserveCloseIdler(
+			Decorator{
+				RoundTripper: next,
+				CloseIdler:   closeIdler,
+			},
+			Func(func(r *http.Request) (*http.Response, error) {
+				return next.RoundTrip(r)
+			}),
+		)
+	)
+
+	suite.Require().NotNil(decorator)
+	next.On("RoundTrip", suite.request).Once().Return(suite.response, suite.err)
+	closeIdler.On("CloseIdleConnections").Once()
+
+	response, err := decorator.RoundTrip(suite.request)
+	suite.Equal(suite.response, response)
+	suite.Equal(suite.err, err)
+
+	suite.Require().Implements((*CloseIdler)(nil), decorator)
+	decorator.(CloseIdler).CloseIdleConnections()
+
+	next.AssertExpectations(suite.T())
+	closeIdler.AssertExpectations(suite.T())
+}
+
+func TestPreserveCloseIdler(t *testing.T) {
+	suite.Run(t, new(PreserveCloseIdlerTestSuite))
 }
