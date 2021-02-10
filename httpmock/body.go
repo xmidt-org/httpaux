36bfe27b5b55653be82594a4aa4f5d9e54f58210
johnabass <johnbass1017@yahoo.com>
2021-02-10 14:59:13 -0800

    tests for the Body functions

--- httpmock/body.go
+++ httpmock/body.go
@@ -11,16 +11,16 @@ func EmptyBody() io.ReadCloser {
 	return BodyBytes(nil)
 }
 
-// BodyString is syntactic sugar for creating a response body from a string
-func BodyString(b string) io.ReadCloser {
-	return ioutil.NopCloser(
-		bytes.NewBufferString(b),
-	)
-}
-
 // BodyBytes is syntactic sugar for creating a response body from a byte slice
 func BodyBytes(b []byte) io.ReadCloser {
 	return ioutil.NopCloser(
 		bytes.NewBuffer(b),
+	)
+}
+
+// BodyString is syntactic sugar for creating a response body from a string
+func BodyString(b string) io.ReadCloser {
+	return ioutil.NopCloser(
+		bytes.NewBufferString(b),
 	)
 }
