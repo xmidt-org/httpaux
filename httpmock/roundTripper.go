25d11bde71c408af90d8ec9dd892f8b24f8ea6d6
johnabass <johnbass1017@yahoo.com>
2021-03-03 10:38:59 -0800

    comments

--- httpmock/roundTripper.go
+++ httpmock/roundTripper.go
@@ -6,12 +6,12 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-// RoundTripper is a mocked http.RoundTripper
+// RoundTripper is a mocked http.RoundTripper.
 type RoundTripper struct {
 	mock.Mock
 }
 
-// RoundTrip implements http.RoundTripper and is driven by the mock's expectations
+// RoundTrip implements http.RoundTripper and is driven by the mock's expectations.
 func (m *RoundTripper) RoundTrip(request *http.Request) (*http.Response, error) {
 	var (
 		arguments = m.Called(request)
@@ -22,18 +22,18 @@ func (m *RoundTripper) RoundTrip(request *http.Request) (*http.Response, error) 
 	return first, err
 }
 
-// RoundTripCall is syntactic sugar around a RoundTrip *mock.Call
+// RoundTripCall is syntactic sugar around a RoundTrip *mock.Call.
 type RoundTripCall struct {
 	*mock.Call
 }
 
-// Return establishes the return values for this RoundTrip invocation
+// Return establishes the return values for this RoundTrip invocation.
 func (rtc RoundTripCall) Return(r *http.Response, err error) RoundTripCall {
 	rtc.Call.Return(r, err)
 	return rtc
 }
 
-// OnRoundTrip starts a *mock.Call fluent chain to define an expectation
+// OnRoundTrip starts a *mock.Call fluent chain to define an expectation.
 //
 // The expected parameter is an interface{} allow not only a *http.Request
 // but also a MatchedBy predicate.
@@ -43,17 +43,17 @@ func (m *RoundTripper) OnRoundTrip(expected interface{}) RoundTripCall {
 	}
 }
 
-// CloseIdler is a mocked httpaux.CloseIdler that also mocks http.RoundTripper
+// CloseIdler is a mocked httpaux.CloseIdler that also mocks http.RoundTripper.
 type CloseIdler struct {
 	RoundTripper
 }
 
-// CloseIdleConnections implements httpaux.CloseIdler and is driven by this mock's expectations
+// CloseIdleConnections implements httpaux.CloseIdler and is driven by this mock's expectations.
 func (m *CloseIdler) CloseIdleConnections() {
 	m.Called()
 }
 
-// OnCloseIdleConnections starts a fluent chain for defining a CloseIdleConnections expectation
+// OnCloseIdleConnections starts a fluent chain for defining a CloseIdleConnections expectation.
 func (m *CloseIdler) OnCloseIdleConnections() *mock.Call {
 	return m.On("CloseIdleConnections")
 }
