bc40131bc8740abf4c3a90baaf370acab5b292ca
johnabass <johnbass1017@yahoo.com>
2021-03-29 22:12:06 -0700

    fixed a misspelling

--- erraux/errorFielder.go
+++ erraux/errorFielder.go
@@ -51,7 +51,7 @@ func (f Fields) Merge(more Fields) {
 // Add adds a variadic set of names and values to this fields.
 //
 // Each even-numbered item in this method's variadic arguments must be a string, or
-// this method will panic.  Each odd-numbered item is paired as the value of the preceeding
+// this method will panic.  Each odd-numbered item is paired as the value of the preceding
 // name.  If there are an odd number of items, the last item must be a string and it
 // is interpreted as having an empty value.
 func (f Fields) Add(namesAndValues ...interface{}) {
