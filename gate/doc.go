1b494ef02fc1aba864c30e3b13c8dbd8ee9548b0
johnabass <johnbass1017@yahoo.com>
2024-07-15 17:29:49 -0700

    chore: added SPDX comment block

--- gate/doc.go
+++ gate/doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
 /*
 Package gate implements a simple atomic boolean that controls access to HTTP client
 or server functionality
